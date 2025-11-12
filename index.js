0435cc9ac3630f1421a664119091ec2aa0eb996c
MostofaKobirAdon <mostofakobiradon9@gmail.com>
2025-11-12 21:55:13 +0600

    added functionality to remove a habit data from database

--- index.js
+++ index.js
@@ -61,6 +61,13 @@ async function run() {
       res.send(result);
     });
 
+    app.delete("/habits/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await habitsCollection.deleteOne(query);
+      res.send(result);
+    });
+
     app.get("/habits/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
